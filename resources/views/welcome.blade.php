916e08b023d110e5cda467ae0deb714bb26b5a10
sonucjha <nabnitcjha@gmil.com>
2024-09-02 02:17:01 +0545

    welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,8 @@
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:justify-center lg:col-start-2 page-title">
-                            SONU FAMILY
+                        <div class="flex lg:justify-center lg:col-start-2">
+                            @include('welcome_page_title')
                         </div>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
