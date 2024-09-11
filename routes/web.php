17ce7a86480d0fbb5297c0732d6f2a1cec9d070b
sonucjha <nabnitcjha@gmil.com>
2024-09-11 12:19:30 +0545

    improve code

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,24 @@ Route::get('/see-betu-photo', function () {
 Route::get('/upload-betu-photo', function () {
     return view('BETU_PHOTO.upload_photo');
 });
+
+// ============================================================================
+
 Route::get('/see-betu-video', function () {
     return view('BETU_VIDEO.see_video');
 });
 Route::get('/upload-betu-video', function () {
     return view('BETU_VIDEO.upload_video');
 });
-Route::get('/betu-cv', function () {
-    return view('betu_cv');
-});
 
+// ============================================================================
 Route::get('/see-cv', function () {
-    return view('see_cv');
+    return view('BETU_CV.see_cv');
 });
 Route::get('/upload-cv', function () {
-    return view('upload_cv');
+    return view('BETU_CV.upload_cv');
 });
+// =============================================================================
 
 Route::get('/see-family-photo', function () {
     return view('FAMILY_PHOTO.see_photo');
@@ -41,6 +43,9 @@ Route::get('/see-family-photo', function () {
 Route::get('/upload-family-video', function () {
     return view('FAMILY_PHOTO.upload_photo');
 });
+
+// =============================================================================
+
 Route::get('/see-family-video', function () {
     return view('FAMILY_VIDEO.see_video');
 });
