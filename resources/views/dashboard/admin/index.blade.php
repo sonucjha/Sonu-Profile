f497f98c3dd1f7f804d341b4191bbea50c8d5c50
sonucjha <nabnitcjha@gmil.com>
2024-09-12 10:24:00 +0545

    improve code

--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -8,13 +8,13 @@
         <div class="container-fluid">
 
             <!-- start page title -->
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col-12 admin-start-title">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                         <h4 class="mb-sm-0" style="font-size: 40px;color:#01030299">KRIASHIK BETU ======> SON OF SONU</h4>
                     </div>
                 </div>
-            </div>
+            </div> --}}
             <!-- end page title -->
             <div class="row">
                 <div class="col-xl-12">
